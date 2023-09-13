52c6e69562edc7c313fd8b95d0c5b612620c4083
ignacio.martinez <50220027+rocogamer@users.noreply.github.com>
2023-09-13 15:02:33 +0200

    Mejora en la recogida del tipo de nodo

--- models/node.py
+++ models/node.py
@@ -2,14 +2,18 @@ import os
 
 
 class Node:
-    def __init__(self, node_type, node_name="NA", node_ip="NA"):
-        if node_type.lower() != "master" and node_type.lower() != "worker":
+    def __init__(self, config = None, node_type = "NA", node_name="NA", node_ip="NA"):
+        if node_type != "NA" and node_type.lower() != "master" and node_type.lower() != "worker":
             raise Exception("Node type must be master or worker")
-
-        self.node_type = node_type.lower()
+        if node_type == "NA" and config is None and type(config).__name__ != "ConfigParser":
+            raise Exception("Node type must be master or worker or config bust be ConfigParser object")
 
         self.node_name = os.popen("hostname").read().strip() if node_name == "NA" else node_name
         self.node_ip = os.popen("hostname -I").read().strip() if node_ip == "NA" else node_ip
+
+        if node_type == "NA" and config is not None:
+            self.node_type = config[f"node_{self.node_name}"]["node_type"]
+
 
     def execute_command(self, command):
         os.system(command)
