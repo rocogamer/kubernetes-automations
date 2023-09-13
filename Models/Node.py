327ddb399209c3f0265dd00409c32d363369d2b9
ignacio.martinez <50220027+rocogamer@users.noreply.github.com>
2023-09-13 02:18:35 +0200

    Acabamos de ajustar el case-insensitive a la hora de crear un nodo

--- Models/Node.py
+++ Models/Node.py
@@ -6,7 +6,7 @@ class Node:
         if node_type.lower() != "master" and node_type.lower() != "worker":
             raise Exception("Node type must be master or worker")
 
-        self.node_type = node_type
+        self.node_type = node_type.lower()
 
         self.node_name = os.popen("hostname").read().strip() if node_name == "NA" else node_name
         self.node_ip = os.popen("hostname -I").read().strip() if node_ip == "NA" else node_ip
